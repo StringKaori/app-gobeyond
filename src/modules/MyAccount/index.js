bc3921efcf1c92953b45efdc2b7d348f52f0cdc3
Guilherme Leite <guilherme99leite@gmail.com>
2021-01-12 19:30:38 -0300

    adicionados AppNavigator e index em Home e MyAccount, ambos indexes linkados com o app navigator

---
+++ src/modules/MyAccount/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import {Text, View } from 'react-native';
+
+
+const MyAccount = () => {
+  return (
+    <View>
+        <Text>My Account</Text>
+    </View>
+  );
+}
+
+export default MyAccount;
