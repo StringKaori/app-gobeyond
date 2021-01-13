7ee146f2d6f1c22ac8ea674592895955d41a2d3b
Guilherme Leite <guilherme99leite@gmail.com>
2021-01-13 18:00:53 -0300

    criadas estruturas de navegacao

--- src/modules/Home/index.js
+++ src/modules/Home/index.js
@@ -1,13 +1,38 @@
 import React from 'react';
-import {Text, View } from 'react-native';
-
+import {Text, View, TouchableOpacity, StyleSheet } from 'react-native';
+import { useNavigation } from '@react-navigation/native';
 
 const Home = () => {
+
+  const { navigate } = useNavigation();
+
   return (
-    <View>
+    <View style={{flex:1, alignItems:'center'}}>
         <Text>Home</Text>
+
+        <View style={{flex:1, alignItems:'center', justifyContent:'center'}}>
+          <TouchableOpacity style={styles.botao} onPress={() => {navigate('MyAccount')}}>
+            <Text style={styles.textoBotao}>Ir para Perfil</Text>
+          </TouchableOpacity>
+        </View>
+
     </View>
   );
 }
+
+
+const styles = StyleSheet.create ({
+  botao:{
+    width: 150,
+    height: 50,
+    backgroundColor:'#4971fe',
+    alignItems:'center',
+    justifyContent:'center'
+  },
+  textoBotao:{
+    color:'#fff',
+  }
+});
+
 
 export default Home;
